2d7f748f5931928220e96d2360b9f38ceeeac2ba
LTOriane <oriane.letiran@gmail.com>
2023-05-11 12:53:52 +0200

    New js file for fetch and index products loading

---
+++ front/js/script.js
@@ -0,0 +1,38 @@
+//Fetch API//
+
+async function productFetch() {
+  const response = await fetch("http://localhost:3000/api/products");
+  const data = await response.json();
+  console.log(data);
+
+  //DOM integration//
+
+  let sectionItems = document.getElementById("items");
+
+  for (i = 0; i < data.length; i++) {
+    var section = document.createElement("a");
+    var article = document.createElement("article");
+    var productsImage = document.createElement("img");
+    var productsName = document.createElement("h3");
+    var productsDescription = document.createElement("p");
+
+    section.href = "./product.html?id=" + data[i];
+
+    productsImage.src = data[i].imageUrl;
+    productsImage.setAttribute("alt", data[i].altTxt);
+
+    productsName.setAttribute("class", "productName");
+    productsName.textContent = data[i].name;
+
+    productsDescription.setAttribute("class", "productDescription");
+    productsDescription.textContent = data[i].description;
+
+    article.appendChild(productsImage);
+    article.appendChild(productsName);
+    article.appendChild(productsDescription);
+    section.appendChild(article);
+    sectionItems.appendChild(section);
+  }
+}
+
+productFetch();
